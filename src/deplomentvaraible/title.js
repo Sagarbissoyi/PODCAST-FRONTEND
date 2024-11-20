68fab634bb309856f307168f271917bd1e08310c
sagarbissoyi <sagarbissoyi10@gmail.com>
2024-11-20 10:19:46 +0530

    fix

--- src/deplomentvaraible/title.js
+++ src/deplomentvaraible/title.js
@@ -1,5 +1,5 @@
-// let deploylink="https://podcast-backend-9t5w.onrender.com"
-let deploylink = "http://localhost:1000"
+let deploylink="https://podcast-backend-9t5w.onrender.com"
+// let deploylink = "http://localhost:1000"
 export default deploylink;
 
 /////${deploylink}
