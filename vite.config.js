dc4efb309fd19ecbc044ae0bdf80d1a6c3337bf9
sagarbissoyi <sagarbissoyi10@gmail.com>
2024-11-20 18:00:15 +0530

    fix 404 not found

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base:"/",
   plugins: [react()],
-  root: '.', // Set the root directory to the current folder
-  build: {
-    outDir: 'dist', // Ensure the build files go to `dist`
-    rollupOptions: {
-      input: './index.html', // Specify the correct location of index.html
-    },
-  },
-});
+
+})
+
+
+
+
+
+
